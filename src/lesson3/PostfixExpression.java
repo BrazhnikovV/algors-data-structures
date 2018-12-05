3de153affcd55710c155465ec758f0f748513482
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-12-05 10:18:32 +0300

    Докблоки и коментарии для PostfixExpression.

--- src/lesson3/PostfixExpression.java
+++ src/lesson3/PostfixExpression.java
@@ -2,22 +2,44 @@ package lesson3;
 
 import java.util.NoSuchElementException;
 
+/**
+ * PostfixExpression - класс для вычисления выражения в польской записи
+ *
+ * @version 1.0.1
+ * @package lesson3
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
 public class PostfixExpression {
 
+    /**
+     * @access private
+     * String expr - исходный выражение
+     */
     private String expr;
 
+    /**
+     * PostfixExpression - конструктор
+     * @param expr - выражения для вычисления
+     */
     public PostfixExpression ( String expr ) {
         this.expr = expr;
     }
 
+    /**
+     * calcExpression - вычислить выражения в польской записи
+     * @return int
+     */
     public int calcExpression () {
 
         MyArrayStack<Integer> stack = new MyArrayStack<>();
 
         String[] s = expr.split( " " );
 
-        for (int i = 0; i < s.length; i++) {
-            if ( s[i].equals("+") || s[i].equals("-") || s[i].equals("/") || s[i].equals("*") ) {
+        for ( int i = 0; i < s.length; i++ ) {
+
+            if ( s[i].equals( "+" ) || s[i].equals( "-" ) || s[i].equals( "/" ) || s[i].equals( "*" ) ) {
+
                 if ( stack.size() < 2 ) {
                     throw new NoSuchElementException();
                 }
@@ -25,21 +47,13 @@ public class PostfixExpression {
                 int a = stack.pop();
                 int b = stack.pop();
 
-                if ( s[i].equals("+") ) {
-                    stack.push(a+b);
-                }
-                else if ( s[i].equals("-") ) {
-                    stack.push(b+a);
-                }
-                else if ( s[i].equals("*") ) {
-                    stack.push(a*b);
-                }
-                else if ( s[i].equals("/") ) {
-                    stack.push(b/a);
-                }
+                if      ( s[i].equals( "+" ) ) { stack.push(a + b ); }
+                else if ( s[i].equals( "-" ) ) { stack.push(b + a ); }
+                else if ( s[i].equals( "*" ) ) { stack.push(a * b ); }
+                else if ( s[i].equals( "/" ) ) { stack.push(b / a ); }
             }
             else {
-                stack.push( Integer.parseInt(s[i]));
+                stack.push( Integer.parseInt( s[i]) );
             }
         }
 
