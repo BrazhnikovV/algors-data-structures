b363b15385b338fe1d6a8a52b0e1c45486f804ad
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-12-07 17:54:46 +0300

    Разбор кода лекции №4. MyLinkedStack докблоки.

--- src/lesson4/MyLinkedStack.java
+++ src/lesson4/MyLinkedStack.java
@@ -1,25 +1,57 @@
 package lesson4;
 
+/**
+ * MyLinkedStack -
+ *
+ * @version 1.0.1
+ * @package lesson4
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
 class MyLinkedStack<Item> {
 
+    /**
+     * @access private
+     * MyLinkedList<Item> stack - стек
+     */
     private MyLinkedList<Item> stack = new MyLinkedList<>();
 
+    /**
+     * push -
+     * @param item - вставляемый элемент
+     */
     public void push ( Item item ) {
         this.stack.insertFirst( item );
     }
 
+    /**
+     * pop -
+     * @return Item
+     */
     public Item pop () {
         return this.stack.deleteFirst();
     }
 
+    /**
+     * peek -
+     * @return Item
+     */
     public Item peek () {
         return this.stack.getFirst();
     }
 
+    /**
+     * size -
+     * @return
+     */
     public int size () {
         return this.stack.size();
     }
 
+    /**
+     * isEmpty -
+     * @return boolean
+     */
     public boolean isEmpty () {
         return this.stack.isEmpty();
     }
